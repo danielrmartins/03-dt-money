ff4e0e97b1661d957d3249345b4d40dbb96bd8cc
Daniel <danielmartins10@gmail.com>
2024-09-27 10:51:48 -0300

    chore: downgrade `json-server`, install `axios`

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,15 +1,20 @@
 import { MagnifyingGlass } from "phosphor-react";
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 import { SearchFormContainer } from "./styles";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 
 const searchFormSchema = z.object({ query: z.string() });
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext);
+
   const {
     register,
     handleSubmit,
@@ -17,9 +22,7 @@ export function SearchForm() {
   } = useForm<SearchFormInputs>({ resolver: zodResolver(searchFormSchema) });
 
   async function handleSearchTransactions(data: SearchFormInputs) {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    console.log(data);
+    await fetchTransactions(data.query);
   }
 
   return (
--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,5 +1,7 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 
+import { api } from "../lib/axios";
+
 interface Transaction {
   id: number;
   description: string;
@@ -10,6 +12,7 @@ interface Transaction {
 }
 interface TransactionContextType {
   transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 interface TransactionsProviderProps {
   children: ReactNode;
@@ -19,15 +22,17 @@ export const TransactionsContext = createContext({} as TransactionContextType);
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3333/transactions");
-    const data = await response.json();
-    setTransactions(data);
+  async function fetchTransactions(query?: string) {
+    const response = await api.get("/transactions", { params: { q: query } });
+
+    setTransactions(response.data);
   }
 
   useEffect(() => {
-    loadTransactions();
+    fetchTransactions();
   }, []);
 
-  return <TransactionsContext.Provider value={{ transactions }}>{children}</TransactionsContext.Provider>;
+  return (
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>{children}</TransactionsContext.Provider>
+  );
 }
